1ff55df4a3731cb101c6cf5a11941c652afa030a
Saurav Nagpal <snagpal@appcelerator.com>
2013-12-31 12:44:09 +0530

    Implement the generic method to add element in a list.

--- C Link List/LinkList.C
+++ C Link List/LinkList.C
@@ -7,6 +7,7 @@
 //
 
 #include "LinkList.h"
+#include "string.h"
 #include <stdlib.h>
 
 /*************************
@@ -25,20 +26,31 @@ struct list_struct_info{
 struct link_list_struct_info{
     list_struct_info*       link_list;
     long                    list_element_count;
+    link_list_type          list_type;
 };
+
+/*************************
+ *Function Declaration
+ ************************/
+list_struct* newListElement(link_list_type type,void* data);
 
 /*************************
  *Function Defination
  ************************/
 
+/*************************
+ *Public Methods
+ ************************/
 //allocate the memory to list
-link_list* newLinkList(){
+link_list* newLinkList(link_list_type type){
     link_list* newList = (link_list*) calloc(sizeof(link_list), 1);
     newList->link_list = NULL;
     newList->list_element_count = 0;
+    newList->list_type = type;
     return newList;
 }
 
+//release the link list memory
 void releaseLinkList(link_list** ptrToList){
     list_struct_info* list = (*ptrToList)->link_list;
     list_struct_info* tmpList = NULL;
@@ -50,22 +62,25 @@ void releaseLinkList(link_list** ptrToList){
     *ptrToList = NULL;
 }
 
+//return the element count
+long getElementCount(link_list* list){
+    return list->list_element_count;
+}
+
 //add the element to the list
 void addElementInLinkList(link_list* listContainer, void* data){
-    list_struct_info *list_element = (list_struct_info*) calloc(sizeof(list_struct_info), 1);
-    list_struct_info* list = listContainer->link_list;
-    list_element->data = data;
-    list_element->next = NULL;
+    list_struct* list = listContainer->link_list;
+    list_struct* newElement = newListElement(listContainer->list_type, data);
     switch (listContainer->list_element_count) {
         case 0:{
-            listContainer->link_list = list_element;
+            listContainer->link_list = newElement;
             break;
         }
         default:{
              while (list->next != NULL) {
                  list = list->next;
              }
-            list->next = list_element;
+            list->next = newElement;
             break;
         }
     }
@@ -78,4 +93,43 @@ void deleteElementInLinkList(link_list* listContainer, void* data){
     while (list->next != NULL) {
         list = list->next;
     }
+}
+
+/*************************
+ *Private Methods
+ ************************/
+list_struct* newListElement(link_list_type type,void* data){
+    list_struct *list_element = (list_struct*) calloc(sizeof(list_struct), 1);
+    switch (type) {
+        case link_list_int:{
+            int* newData = (int*)calloc(sizeof(int), 1);
+            *newData =*((int*)data);
+            list_element->data = newData;
+            break;
+        }
+        case link_list_double:{
+            double* newData = (double*)calloc(sizeof(double), 1);
+            *newData =*((double*)data);
+            list_element->data = newData;
+            break;
+        }
+        case link_list_float:{
+            float* newData = (float*)calloc(sizeof(float), 1);
+            *newData =*((float*)data);
+            list_element->data = newData;
+            break;
+        }
+        case link_list_string:{
+            char* newData = (char*)calloc(sizeof(char), strlen((char*)data));
+            *newData =*((char*)data);
+            list_element->data = newData;
+            break;
+        }
+        case link_list_void:{
+            list_element->data = data;
+            break;
+        }
+    }
+    list_element->next = NULL;
+    return list_element;
 }
--- C Link List/LinkList.h
+++ C Link List/LinkList.h
@@ -9,12 +9,21 @@
 #ifndef C_Link_List_LinkList_h
 #define C_Link_List_LinkList_h
 
+typedef enum link_list_type_enum{
+    link_list_int,
+    link_list_float,
+    link_list_double,
+    link_list_string,
+    link_list_void // if specify then memory handling at developer end
+}link_list_type;
+
 typedef struct link_list_struct_info link_list;
 
-link_list* newLinkList();
+link_list* newLinkList(link_list_type type);
 void releaseLinkList(link_list** list);
+long getElementCount(link_list* list);
 void addElementInLinkList(link_list* list, void* data);
-void deleteElementInLinkList(link_list* listContainer, void* data);
+void deleteElementInLinkList(link_list* list, void* data);
 void deleteElementAtIndexInLinkList(int index);
 
 #endif
